407f95df2297c32d7349d760512f62668fcf6692
konishi333 <jobhuntingkonishi3@gmail.com>
2023-09-18 18:13:49 +0900

    ログインのバリデーション修正
    新規作成のバリデーションも修正している

--- routes/user.js
+++ routes/user.js
@@ -13,36 +13,54 @@ router.get("/login", (req, res) => {
       return;
     }
 
-    const message = [];
+    const errors = [];
     const { email } = {};
-    res.render("user/login", { message, email })
+    res.render("user/login", { errors, email })
   } catch (error) {
     console.log(error);
   }
 });
 
-router.post("/login-check", async (req, res) => {
-  try {
-    const email = req.body.email;
-    const user = await userModel.findOne({email: email});
-    if (!user) {
-      const message = "EmailまたはPasswordが誤っています。"
-      res.render("user/login", { email, message });
+router.post(
+  "/login-check",
+  [
+    body("email")
+      .trim()
+      .notEmpty().withMessage("メールアドレスを入力してください")
+      .custom(async (value, {req}) => {
+        const user = await userModel.findOne({email: value});
+        if (!user) {
+          throw new Error("EmailまたはPasswordが誤っています。");
+        }
+      }),
+    body("password").trim().notEmpty().withMessage("パスワードを入力してください"),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const { email } = req.body;
+        res.render("user/login", { email, errors: errors.array() });
+        return;
+      }
+
+      const email = req.body.email;
+      const user = await userModel.findOne({email: email});
+      const password = req.body.password;
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (isPasswordMatch) {
+        req.session.email = email;
+        res.redirect("/");
+      } else {
+        const message = "EmailまたはPasswordが誤っています.";
+        const loginErrors = [...errors.array(), { msg: message }];
+        res.render("user/login", { email, errors: loginErrors });
+      }
+    } catch (error) {
+      res.status(500).send('サーバーエラーが発生しました。');
     }
-    
-    const password = req.body.password;
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (isPasswordMatch) {
-      req.session.email = email;
-      res.redirect("/");
-    } else {
-      const message = "EmailまたはPasswordが誤っています。"
-      res.render("user/login", { email, message });
-    }
-  } catch (error) {
-    res.status(500).send('サーバーエラーが発生しました。');
   }
-});
+);
 
 router.get("/index", async (req, res) => {
   try {
@@ -75,9 +93,24 @@ router.get("/new", (req, res) => {
 
 router.post("/create",
   [
-    body("name").trim().isLength({ max: 20 }).withMessage("ユーザー名は20文字以内で入力してください"),
-    body("email").trim().isLength({ max: 60 }).withMessage("メールアドレスは60文字以内で入力してください"),
-    body("password").trim().isLength({ min: 8 }).withMessage("パスワードは8文字以上で入力してください"),
+    body("name")
+      .trim()
+      .notEmpty().withMessage("名前を入力してください")
+      .isLength({ max: 20 }).withMessage("ユーザー名は20文字以内で入力してください"),
+    body("email")
+      .trim()
+      .notEmpty().withMessage("メールアドレスを入力してください")
+      .isLength({ max: 60 }).withMessage("メールアドレスは60文字以内で入力してください"),
+    body("password")
+      .trim()
+      .custom((value, {req}) => {
+        if (value.length === 0) {
+          throw new Error("パスワードを入力してください")
+        } else if (value.length < 8) {
+          throw new Error("パスワードは8文字以上で入力してください")
+        }
+        return true;
+      })
   ],
   async (req, res) => {
     try {
