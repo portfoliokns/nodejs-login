ed41e03bb9bf8920cd4c51d0e12d61b0401f40d7
konishi333 <jobhuntingkonishi3@gmail.com>
2023-09-22 15:40:00 +0900

    メールアドレス単位での取得機能を実装

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../models/user");
+const api_key = "890890890";
 
 router.use(express.json());
 
 router.get("/user",async (req, res) => {
   try {
-    const api_key = req.headers.api_key;
-    if (api_key !== "890890890") {
+    const get_key = req.headers.api_key;
+    if (!checkApiKey(get_key)) {
       res.redirect("/not-found");
+      return;
     }
-
     const user = await userModel.find({}, 'name email');
-    res.json(user);
+    if (user) {
+      res.json(user);
+    } else {
+      res.json("該当する情報はありませんでした");
+    }
   } catch (error) {
     console.log(error);
   }
 });
+
+router.get("/user/:email",async (req, res) => {
+  try {
+    const get_key = req.headers.api_key;
+    if (!checkApiKey(get_key)) {
+      res.redirect("/not-found");
+      return;
+    }
+    const email = req.params.email;
+    const user = await userModel.findOne({email: email}, 'name email');
+    if (user) {
+      res.json(user);
+    } else {
+      res.json("該当する情報はありませんでした");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+function checkApiKey (get_key) {
+  if (api_key === get_key) {
+    return true;
+  }
+  return false;
+};
 
 module.exports = router
